8a803bd4abc56d2d5b88361ad4b004a5bb45c4a9
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-04-16 11:04:59 +0300

    Refactor tests to ensure all classes have been initialized and instantiated correctly
    This includes adding a test function as a manager command in the manage.py file. The test results must all be correct as follows
    test_init (test_article.ArticleTest) ... ok
    test_instance (test_article.ArticleTest) ... ok
    test_init (test_source.SourceTest) ... ok
    test_instance (test_source.SourceTest) ... ok

--- tests/test_article.py
+++ tests/test_article.py
@@ -10,7 +10,7 @@ class ArticleTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_article =("Crystal Bell","BTS And Halsey Kick Off Summer Early With 'Boy With Luv' - MTV.com", "Oh my my my oh my my my' prepare to have this song stuck in your head","http://www.mtv.com/news/3120451/bts-halsey-boy-with-luv-video/","https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:671751?quality=0.8&format=jpg&width=1440&height=810&.jpg","2019-04-12T10:55:00Z","Five years ago, BTS were a group of young adults in their teens and early twenties trying to make sense of the world around them. They channeled these confusing coming-of-age emotions and experiences like first love, disappointment, and heartbreak into their...","source.name: MTV News"})
+        self.new_article =Article("Crystal Bell","BTS And Halsey Kick Off Summer Early With 'Boy With Luv' - MTV.com", "Oh my my my oh my my my' prepare to have this song stuck in your head","http://www.mtv.com/news/3120451/bts-halsey-boy-with-luv-video/","https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:671751?quality=0.8&format=jpg&width=1440&height=810&.jpg","2019-04-12T10:55:00Z","Five years ago, BTS were a group of young adults in their teens and early twenties trying to make sense of the world around them. They channeled these confusing coming-of-age emotions and experiences like first love, disappointment, and heartbreak into their...")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_article,Article))
@@ -27,7 +27,7 @@ class ArticleTest(unittest.TestCase):
         self.assertEqual(self.new_article.urlToImage,"https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:671751?quality=0.8&format=jpg&width=1440&height=810&.jpg")
         self.assertEqual(self.new_article.publishedAt,"2019-04-12T10:55:00Z")
         self.assertEqual(self.new_article.content,"Five years ago, BTS were a group of young adults in their teens and early twenties trying to make sense of the world around them. They channeled these confusing coming-of-age emotions and experiences like first love, disappointment, and heartbreak into their...")
-        self.assertEqual(self.new_article.source.name,"source.name: MTV News"})
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_source.py
+++ tests/test_source.py
@@ -10,7 +10,7 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = ("abc-news","ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","general","en","us")
+        self.new_source = Source("abc-news","ABC News","Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.","https://abcnews.go.com","general","en","us")
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
@@ -27,6 +27,3 @@ class SourceTest(unittest.TestCase):
         self.assertEqual(self.new_source.category,"general")
         self.assertEqual(self.new_source.language,"en")
         self.assertEqual(self.new_source.country,"us")
-
-if __name__ == '__main__':
-    unittest.main()
